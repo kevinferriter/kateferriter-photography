c2f4194799d90635724e84bdcdef58d8f5f287c4
Kevin Ferriter <kevinferriter@gmail.com>
2026-06-04 09:58:01 -0400

    moved navigation.ts file

---
+++ navigation.ts
@@ -0,0 +1,26 @@
+// import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    { text: 'Home', href: '/' },
+    { text: 'About', href: '/about' },
+    { text: 'Portfolio', href: '/portfolio' },
+    { text: 'Contact', href: '/contact' },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Pages',
+      links: [
+        { text: 'Home', href: '/' },
+        { text: 'About', href: '/about' },
+        { text: 'Portfolio', href: '/portfolio' },
+        { text: 'Contact', href: '/contact' },
+      ],
+    },
+  ],
+  secondaryLinks: [],
+  socialLinks: [],
+};
